1e9629d70dba28c2774754f8deab1d25920d5fcc
Matias Charrut <mcharrut@fi.uba.ar>
2021-10-15 12:19:16 -0300

    Agregada BlockingQueue

---
+++ blocking_queue.h
@@ -0,0 +1,46 @@
+#ifndef TP3_TALLER2_BLOCKING_QUEUE_H
+#define TP3_TALLER2_BLOCKING_QUEUE_H
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+template <class T>
+class BlockingQueue {
+private:
+	std::queue<T> queue;
+	std::mutex mutex;
+	std::condition_variable cv;
+	
+public:
+	BlockingQueue<T>();
+	
+	void push(const T & new_element);
+	
+	T pop();
+};
+
+template <class T>
+BlockingQueue<T>::BlockingQueue(): queue(), mutex(), cv() {}
+
+template <class T>
+void BlockingQueue<T>::push(const T & new_element) {
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+		queue.push(new_element);
+	}
+	cv.notify_one();
+}
+
+template <class T>
+T BlockingQueue<T>::pop() {
+	std::unique_lock<std::mutex> lock(mutex);
+	cv.wait(lock, [=] {
+		return !this->queue.empty();
+	});
+	T ret(queue.front());
+	queue.pop();
+	return ret;
+}
+
+#endif //TP3_TALLER2_BLOCKING_QUEUE_H
